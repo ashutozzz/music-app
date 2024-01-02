bbd8858571ede43e6294c0ffb4afbb8ebf29764c
Ashutosh Dubey <ashutozzz09@gmail.com>
2024-01-02 20:09:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Grid from "./Component/Grid/Grid";
+import Login from "./Component/Login/Login";
+import Signup from "./Component/Signup/Signup";
+import List from "./Component/List/List";
+import Description from "./Component/Description/Description";
+import Cart from "./Component/Cart/Cart";
+import Checkout from "./Component/Checkout/Checkout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Successfull from "./Component/Successfull/Successfull";
+const App = () => {
+  const [rediect, setrediect] = useState(false);
+  const [first, setfirst] = useState({
+    productName: "",
+    brandName: "",
+    header: "",
+    rating: "",
+    price: "",
+    color: "",
+    type: "",
+    describe: "",
+    available: "",
+  });
+  const [images, setimages] = useState([])
+   const [totalorder, settotalorder] = useState(0 || 0)
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Grid setfirst={setfirst}  setimages={setimages} pushRedirect={rediect}/>} />
+          <Route path="/List" element={<List  setfirst={setfirst}  setimages={setimages} pushRedirect={rediect}/>} />
+          <Route
+            path="/Description"
+            element={<Description sendFirstData={first}  sendimages={images} pushRedirect={rediect}/>}
+          />
+          <Route path="/Cart" element={<Cart  sendFirstData={first}  sendimages={images} settotalorder={settotalorder}/>} />
+          <Route path="/Checkout" element={<Checkout  sendFirstData={first}  sendimages={images} sendtotalorder={totalorder}/>} />
+          <Route path="/Successfull" element={<Successfull />} />
+          <Route path="/Signup" element={<Signup setrediect={setrediect} /> } />
+          <Route path="/login" element={<Login  setrediect={setrediect} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
